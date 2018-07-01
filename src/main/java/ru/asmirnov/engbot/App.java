fba64726d53b8ac0fe768384144a347381e20064
a.smirnov19 <a.smirnov19@tinkoff.ru>
2018-07-01 20:08:33 +0300

    - spring-boot integration - use liquibase yaml format

--- src/main/java/ru/asmirnov/engbot/App.java
+++ src/main/java/ru/asmirnov/engbot/App.java
@@ -2,16 +2,18 @@ package ru.asmirnov.engbot;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.telegram.telegrambots.ApiContextInitializer;
-import ru.asmirnov.engbot.config.AppConfig;
 
 /**
  * Application entry point.
- *
+ * <p>
  * Created by Alexey Smirnov 27.03.2018.
  */
-public class App {
+@SpringBootApplication
+public class App implements CommandLineRunner {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(App.class);
 
@@ -20,9 +22,11 @@ public class App {
         // ApiContext must be initialized here (before start spring context) otherwise BotSessionNotFound will occur
         ApiContextInitializer.init();
         LOGGER.info("Telegram api context initialization OK.");
-        LOGGER.info("Spring context starting...");
-        new AnnotationConfigApplicationContext(AppConfig.class).start();
-        LOGGER.info("Spring context starting OK.");
-        LOGGER.info("Application started.");
+
+        SpringApplication.run(App.class, args);
+    }
+
+    @Override
+    public void run(String... args) {
     }
 }
